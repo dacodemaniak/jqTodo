ef5b87f58d010ce215350f308ad5303c33d49b5b
IDea Factory <dev-team@ideafactory.fr>
2018-08-21 16:51:00 +0200

    Suppressions mutiples et toast

--- assets/javascript/todo.js
+++ assets/javascript/todo.js
@@ -88,6 +88,22 @@ $('#todo').on('submit', function(event) {
 $('tbody').on('click', '.deleteBtn, .multiSelect', function(event) {
     if ($(this).hasClass('deleteBtn')) {
         $(this).parents('tr').remove();
+        // Là, aussi... un petit toast
+        if ($('tbody tr').length == 0) {
+            $.toast(
+                { 
+                    text : 'Tout est fini ! <br> Bravo, bonne journée', 
+                    showHideTransition : 'slide',  // It can be plain, fade or slide
+                    bgColor : 'blue',              // Background color for toast
+                    textColor : '#eee',            // text color
+                    allowToastClose : false,       // Show the close button or not
+                    hideAfter : 3000,              // `false` to make it sticky or time in miliseconds to hide after
+                    stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                    textAlign : 'left',            // Alignment of text i.e. left, right, center
+                    position : 'top-center'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                }
+            );
+        }
     } else {
         // Il s'agit donc d'une boîte à cocher
         if ($('input[type=checkbox]:checked').length > 0) {
@@ -103,4 +119,40 @@ $('tbody').on('click', '.deleteBtn, .multiSelect', function(event) {
             $(this).parent('td').next('td').removeClass('strikedOut');
         }
     }
+});
+
+/**
+ * Suppression multiple des lignes cochées
+ */
+$('#btnMultiDelete').on('click', function(event) {
+    let indice = 0;
+
+    $('tbody tr').each(function() {
+        let firstCol = $(this).children('td').eq(0);
+        let checkbox = firstCol.children('input').eq(0);
+
+        if (checkbox.is(':checked')) {
+            console.log('On dégage la ligne : ' + indice);
+            $(this).remove();
+        }
+        indice++;
+    });
+
+    // En fin de parcours, si plus aucune ligne... journée finie
+    if ($('tbody tr').length == 0) {
+        $.toast(
+            { 
+                text : 'Tout est fini ! <br> Bravo, bonne journée', 
+                showHideTransition : 'slide',  // It can be plain, fade or slide
+                bgColor : 'blue',              // Background color for toast
+                textColor : '#eee',            // text color
+                allowToastClose : false,       // Show the close button or not
+                hideAfter : 3000,              // `false` to make it sticky or time in miliseconds to hide after
+                stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                textAlign : 'left',            // Alignment of text i.e. left, right, center
+                position : 'top-center'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+            }
+        );
+    }
+    $(this).attr('disabled', 'disabled');
 });
