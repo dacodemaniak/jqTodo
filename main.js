ac17a37a0b83f9178137566470d1847c3b2123fa
IDea Factory <dev-team@ideafactory.fr>
2018-08-29 13:48:11 +0200

    Contrôle service worker

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-
+  console.log('Enable service worker');
     // Enregistre un Service Worker
     navigator.serviceWorker.register('/assets/javascript/service-worker.js');
   
